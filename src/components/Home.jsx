548d7a97b3ea44b6173852db79c0eec20c96df2e
Tyler Yates <tyates907@gmail.com>
2021-10-14 16:45:34 -0700

    format file structure & remove cra defaults

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.title}>
+      <h1 className={styles.new}>Home Component</h1>
+    </div>
+  );
+}
