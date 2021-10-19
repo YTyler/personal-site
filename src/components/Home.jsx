16085a520e39c633b9c0680ec6ae65696c39b59f
Tyler Yates <tyates907@gmail.com>
2021-10-19 11:02:00 -0700

    add pictures & style home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,17 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.title}>
-      <h1 className={styles.new}>Home Component</h1>
+    <div className={styles.hero}>
+      <div className={styles.content}>
+        <h1>Title Here</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum amet
+          tenetur vel quis ipsam sequi veniam velit, accusantium quibusdam
+          dolorem dolor tempore, vero nobis autem facilis, quas optio doloremque
+          cupiditate?
+        </p>
+      </div>
+      <img className={styles.profile} src="/profile.jpg" alt="profile_pic" />
     </div>
   );
 }
