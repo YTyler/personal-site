0544cc4fc2df831678ab665628b3d443c90fcd20
Tyler Yates <tyates907@gmail.com>
2021-11-02 14:56:53 -0700

    edit semantic tags

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <section className={`mainRow ${styles.homeContainer}`}>
+    <article className={`mainRow ${styles.homeContainer}`}>
       <div className={styles.content}>
         <h1>Title Here</h1>
         <p>
@@ -21,6 +21,6 @@ export default function Home() {
         </p>
       </div>
       <img className={styles.profilePic} src="/profile.jpg" alt="profile_pic" />
-    </section>
+    </article>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,11 +4,11 @@ import styles from "../styles/Projects.module.css";
 
 export default function Projects() {
   return (
-    <section className={`mainRow ${styles.projects}`}>
+    <article className={`mainRow ${styles.projects}`}>
       <ProjectCard url="/fm.jpg" alttext="fighting mongooses" />
       <ProjectCard url="splash.png" alttext="pop" />
       <ProjectCard url="splash.png" alttext="pop" />
       <ProjectCard url="/fm.jpg" alttext="fighting mongooses" />
-    </section>
+    </article>
   );
 }
