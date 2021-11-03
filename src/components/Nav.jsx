4a67d4d8dcf3a99528873ee7b477dce2d13e1f43
Tyler Yates <tyates907@gmail.com>
2021-11-03 15:37:14 -0700

    add technology icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 import { FaHome, FaFolder, FaFolderOpen, FaPenFancy } from "react-icons/fa";
+import {
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiRedux,
+  SiGithub,
+  SiAmazonaws,
+  SiJest,
+  SiPostman,
+} from "react-icons/si";
 import NavButton from "./NavButton";
 import styles from "../styles/Nav.module.css";
 
@@ -24,10 +34,52 @@ export default function Nav() {
         <FaPenFancy />
       </NavButton>
       <section className={styles.subNav}>
-        <h2>Contact</h2>
+        <h3>Contact</h3>
       </section>
       <section className={styles.subNav}>
-        <h3>Tech</h3>
+        <h3>Technologies</h3>
+        <div className={styles.techIcons}>
+          <div>
+            <a href="https://www.javascript.com/">
+              <SiJavascript id={styles.jsIcon} />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.typescriptlang.org/">
+              <SiTypescript id={styles.tsIcon} />
+            </a>
+          </div>
+          <div>
+            <a href="https://jestjs.io/">
+              <SiJest id={styles.jestIcon} />
+            </a>
+          </div>
+          <div>
+            <a href="https://reactjs.org/">
+              <SiReact id={styles.reactIcon} />
+            </a>
+          </div>
+          <div>
+            <a href="https://redux.js.org/">
+              <SiRedux id={styles.reduxIcon} />
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/">
+              <SiGithub id={styles.gitIcon} />
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/">
+              <SiPostman id={styles.postmanIcon} />
+            </a>
+          </div>
+          <div>
+            <a href="https://aws.amazon.com/">
+              <SiAmazonaws id={styles.awsIcon} />
+            </a>
+          </div>
+        </div>
       </section>
     </nav>
   );
