be9e575c8dc4576ca5c2686c1c034e93014971d6
Tyler Yates <tyates907@gmail.com>
2021-12-28 18:19:17 -0800

    reorganize navbar and restyle

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import { FaHome, FaFolder, FaFolderOpen, FaPenFancy } from "react-icons/fa";
+import React from "react";
+import { MdEmail } from "react-icons/md";
 import {
+  SiLinkedin,
+  SiGithub,
   SiJavascript,
   SiTypescript,
   SiReact,
   SiRedux,
-  SiGithub,
   SiAmazonaws,
   SiJest,
   SiPostman,
+  SiExpo,
 } from "react-icons/si";
-import NavButton from "./NavButton";
 import styles from "../styles/Nav.module.css";
 
 export default function Nav() {
-  const [projectSelected, setProjectSelected] = useState(false);
   return (
     <nav className={`row ${styles.navContainer}`}>
       <section>
@@ -22,65 +22,60 @@ export default function Nav() {
           <h1>Tyler Yates</h1>
           <h2>Web & Native Development</h2>
         </div>
-        <NavButton text="Home">
-          <FaHome />
-        </NavButton>
-        <NavButton
-          text="Projects"
-          onClick={() => setProjectSelected((prevState) => !prevState)} //reversing state with function version
-        >
-          {projectSelected ? <FaFolderOpen /> : <FaFolder />}
-        </NavButton>
-        <NavButton text="Resume">
-          <FaPenFancy />
-        </NavButton>
+        <img
+          className={styles.profilePic}
+          src="/profile.jpg"
+          alt="profile_pic"
+        />
       </section>
       <section className={styles.subNav}>
-        <h3>Contact</h3>
+        <h3>Contact Me</h3>
+        <div className={styles.contactLinks}>
+          <a
+            href="https://www.linkedin.com/in/tyler-yates/"
+            className={styles.contactItem}
+          >
+            <SiLinkedin id={styles.liIcon} />
+            <p className={styles.contactLabel}>LinkedIn: tyler-yates</p>
+          </a>
+
+          <a href="https://github.com/YTyler" className={styles.contactItem}>
+            <SiGithub id={styles.gitIcon} />
+            <p className={styles.contactLabel}>Github: YTyler</p>
+          </a>
+          <div className={styles.contactItem}>
+            <MdEmail id={styles.mailIcon} />
+            <p className={styles.contactLabel}>tyates907@gmail.com</p>
+          </div>
+        </div>
       </section>
       <section className={styles.subNav}>
         <h3>Technologies</h3>
         <div className={styles.techIcons}>
-          <div>
-            <a href="https://www.javascript.com/">
-              <SiJavascript id={styles.jsIcon} />
-            </a>
-          </div>
-          <div>
-            <a href="https://www.typescriptlang.org/">
-              <SiTypescript id={styles.tsIcon} />
-            </a>
-          </div>
-          <div>
-            <a href="https://jestjs.io/">
-              <SiJest id={styles.jestIcon} />
-            </a>
-          </div>
-          <div>
-            <a href="https://reactjs.org/">
-              <SiReact id={styles.reactIcon} />
-            </a>
-          </div>
-          <div>
-            <a href="https://redux.js.org/">
-              <SiRedux id={styles.reduxIcon} />
-            </a>
-          </div>
-          <div>
-            <a href="https://github.com/">
-              <SiGithub id={styles.gitIcon} />
-            </a>
-          </div>
-          <div>
-            <a href="https://www.postman.com/">
-              <SiPostman id={styles.postmanIcon} />
-            </a>
-          </div>
-          <div>
-            <a href="https://aws.amazon.com/">
-              <SiAmazonaws id={styles.awsIcon} />
-            </a>
-          </div>
+          <a href="https://www.javascript.com/" title="javascript">
+            <SiJavascript id={styles.jsIcon} />
+          </a>
+          <a href="https://www.typescriptlang.org/">
+            <SiTypescript id={styles.tsIcon} />
+          </a>
+          <a href="https://jestjs.io/">
+            <SiJest id={styles.jestIcon} />
+          </a>
+          <a href="https://reactjs.org/">
+            <SiReact id={styles.reactIcon} />
+          </a>
+          <a href="https://redux.js.org/">
+            <SiRedux id={styles.reduxIcon} />
+          </a>
+          <a href="https://expo.dev/">
+            <SiExpo id={styles.exIcon} />
+          </a>
+          <a href="https://www.postman.com/">
+            <SiPostman id={styles.postmanIcon} />
+          </a>
+          <a href="https://aws.amazon.com/">
+            <SiAmazonaws id={styles.awsIcon} />
+          </a>
         </div>
       </section>
     </nav>
