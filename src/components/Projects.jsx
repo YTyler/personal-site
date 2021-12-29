1dccabf5b9b5160687c849556700bcce9b4ade07
Tyler Yates <tyates907@gmail.com>
2021-12-29 12:07:04 -0800

    add more project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,16 +24,16 @@ export default function Projects() {
         <ProjectCard
           url="splash.png"
           alttext="pop"
-          title="This Website!"
-          repository="https://github.com/Perfect-Personnel-Placement/frontend"
+          title="My Portfolio Site"
+          repository="https://github.com/YTyler/personal-site"
         >
-          Serverless mobile app used to view client demands and batch output for
-          the company. Designed for Revature Managers to place an order of
-          associates to a demand from a client. I helped to build the AWS API,
-          Lambda handlers and the Relation Database to manipulate and store
-          demand and batch information. I also filled the role of git master;
-          managing branch security/access as well as reviewing and approving
-          pull requests from the other teams.
+          The website you're on right now! Hosted through github pages. Built as
+          a single page app using React and taking advantage of CSS modules.
+          Used in addition to my resume which you can find in the sidebar to the
+          left, along with my contact information and links to several of the
+          technologies I use. I often use this space to test interesting new
+          features and design choices that I find. I hope you're enjoying your
+          time here, and thanks for coming by!
         </ProjectCard>
         <ProjectCard url="splash.png" alttext="pop" />
         <ProjectCard url="/fm.jpg" alttext="fighting mongooses" />
